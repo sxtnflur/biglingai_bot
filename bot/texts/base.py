348096df95200911a7a23d2eac176f6655dfe327
Artyom <sheglove2003@bk.ru>
2025-06-12 00:24:46 +0300

    update prices

--- bot/texts/base.py
+++ bot/texts/base.py
@@ -5,7 +5,7 @@ class BaseTexts:
     START = '''
 Привет, {}! Готов изучать английский?
 
-У тебя <code>{}</code> кредитов{}
+{}
 
 <b>Обучение:</b>
 <code>✏ Чаттинг</code> - <i>Общайся с ИИ текстом на английском и получай обратную связь</i>
@@ -13,7 +13,7 @@ class BaseTexts:
 <code>🔄 Переводчик</code> - <i>Переводи с ИИ: ru -> en и en -> ru</i>
 
 <b>Дополнительно:</b>
-<code>✨ Магазин</code> - <i>Покупай подписки и кредиты для пользования ботом</i>
+<code>🕝 Подписки</code> - <i>Купи подписку и пользуйся всеми  ботом</i>
 <code>💰 Реф. программа</code> - <i>Получай кредиты бесплатно за приглашенных друзей</i>
 '''.strip()
     SUB = '<b>Подписка закончится:</b> <code>{}</code>'
@@ -25,11 +25,11 @@ class BaseTexts:
     CHATTING_BUTTON = '✏ Чаттинг'
     MY_MISTAKES_BUTTON = '🔴 Ошибки'
     TRANSLATOR_BUTTON = '🔄 Переводчик'
-    SUB_AND_CREDITS_BUTTON = '✨ Магазин'
+    SUBS_BUTTON = '🕝 Подписки'
     REF_BUTTON = '💰 Реф. программа'
 
     @staticmethod
     def start(first_name: str, credits: int = 0, sub_end: datetime | None = None):
-        return BaseTexts.START.format(first_name, credits,
+        return BaseTexts.START.format(first_name,
             '\n' + BaseTexts.SUB.format(sub_end.strftime('%H:%M %d.%m.%Y')) if sub_end else ''
         )

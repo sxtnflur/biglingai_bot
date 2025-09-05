fa7cdd64f9344dbfee3683a02e2d1f94e9517da0
Artyom <sheglove2003@bk.ru>
2025-09-05 19:36:39 +0700

    fix

--- bot/callbacks/mistakes.py
+++ bot/callbacks/mistakes.py
@@ -3,7 +3,7 @@ from bot.callbacks.base import ScrollingCallback
 from typing_extensions import Optional
 
 
-class MistakesListCallback(ScrollingCallback, prefix='mistakes-groups-list'):
+class MistakesListCallback(ScrollingCallback, prefix='mgl'):
     ...
 
 
@@ -15,20 +15,20 @@ class MistakeCallback(CallbackData, prefix='mistake'):
     id: int
 
 
-class MistakeGroupListCallback(ScrollingCallback, prefix='mistake-group'):
+class MistakeGroupListCallback(ScrollingCallback, prefix='mg'):
     group: str
 
 
-class TrainMistakeGroupCallback(CallbackData, prefix='train-mistake-group'):
+class TrainMistakeGroupCallback(CallbackData, prefix='tmgr'):
     group: str | None = None
     dialog_uuid: str | None = None
 
 
-class TrainMistakesDialogCallback(CallbackData, prefix='train-mistake-dialog'):
+class TrainMistakesDialogCallback(CallbackData, prefix='tmd'):
     dialog_uuid: str
 
 
-class TrainMistakeGroupAnswerCallback(CallbackData, prefix='train-mistake-group-answer'):
+class TrainMistakeGroupAnswerCallback(CallbackData, prefix='tma'):
     is_right: bool
     mistake_id: int
     group: str | None = None
@@ -37,7 +37,7 @@ class TrainMistakeGroupAnswerCallback(CallbackData, prefix='train-mistake-group-
     @classmethod
     def from_callback_data(cls, callback_data: str) -> Optional['TrainMistakeGroupAnswerCallback']:
         print(f'{callback_data=}')
-        if not callback_data.startswith('train-mistake-group-answer:'):
+        if not callback_data.startswith('tma:'):
             return
 
         try:
@@ -51,7 +51,7 @@ class TrainMistakeGroupAnswerCallback(CallbackData, prefix='train-mistake-group-
             return
 
 
-class DeleteMistakeCallback(CallbackData, prefix='delete-mistake'):
+class DeleteMistakeCallback(CallbackData, prefix='dm'):
     pre: bool = True
     is_worked_out: bool = False
     really_delete: bool = False

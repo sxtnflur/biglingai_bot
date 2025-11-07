a75490e228384f9dbc6c1949446f83848f5285aa
Artyom <sheglove2003@bk.ru>
2025-11-07 23:04:04 +0700

    add redis storage

--- loader.py
+++ loader.py
@@ -1,6 +1,10 @@
 from aiogram import Bot, Dispatcher
 from aiogram.client.default import DefaultBotProperties
+from aiogram.fsm.storage.redis import RedisStorage
+from redis.asyncio import Redis
 from config import settings
 
+
 bot = Bot(token=settings.BOT_TOKEN, default=DefaultBotProperties(parse_mode='HTML'))
+storage = RedisStorage(redis=Redis.from_url(settings.REDIS_URL))
 dp = Dispatcher()
